a645831e0244162cefecb49e8c1c4a6b0a0edec2
Thiago Fernandes <thiagofernandes101@outlook.com>
2021-04-26 18:57:04 -0300

    Teste

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ api.use(cors());
 api.use(express.urlencoded());
 api.use(express.json());
 
-api.use('/api', expressJwt({ secret: process.env.secret || config.secret }).unless({ path: ['/api/usuario/autenticar'] }));
+//api.use('/api', expressJwt({ secret: process.env.secret || config.secret }).unless({ path: ['/api/usuario/autenticar'] }));
 api.use('/api/usuario', require('./controllers/usuario.controller'));
 api.use('/api/perfil', require('./controllers/perfil.controller'));
 api.use('/api/formulario', require('./controllers/formulario.controller'));
